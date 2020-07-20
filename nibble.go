8f1ea71adbe5aa314922a36d6fe7100f37fb1046
Ethan <ethanjhem.e@gmail.com>
2020-07-20 16:44:28 +0100

    Added more features
    -Create Nibbles without having to merge them
    -Added functions to deal with unmerged nibbles
    -Remove necessary bit shift

--- nibble.go
+++ nibble.go
@@ -1,52 +1,76 @@
 package nibble
 
-type DualNum struct {
-	num1 byte
-	num2 byte
-}
-
-func CreateNibble(B *DualNum) byte {
-	num1 := B.num1 << 4
-	num2 := B.num2 << 4
-	num2 = num2 >> 4
-	num3 := num1 | num2 //mix the numbers together
-	return num3
-}
-
-func CreateDualNum(N1 byte, N2 byte) *DualNum {
-	DNUM := new(DualNum)
-	DNUM.num1 = N1
-	DNUM.num2 = N2
+//CreateNibble - Creates a nibble by using half a byte that can be merged with another nibble later
+func CreateNibble(N1 byte) byte {
 	if N1 > 15 {
-		print("Error: num1 over 15 correcting to 15")
-		DNUM.num1 = 15
+		N1 = 15
+	}
+	return N1
+}
+
+//CreateNibbleMerged - Creates a nibble by using 2 bytes (0-15) and merging into byte for space savings in arrays, can be separated with ReadNibbles()
+func CreateNibbleMerged(N1 byte, N2 byte) byte {
+	//prevent values over 15 and correct them to the highest value
+	if N1 > 15 {
+		N1 = 15
 	}
 	if N2 > 15 {
-		print("Error: num2 over 15 correcting to 15")
-		DNUM.num2 = 15
+		N2 = 15
 	}
-	return DNUM
+	N1 = N1 << 4
+	N3 := N1 | N2 //mix the numbers together
+	return N3
 }
 
-func ReadNibble(B byte) (byte, byte) {
+//ReadNibbles - Reads Nibbles in a merged nibble byte and returns the values
+func ReadNibbles(B byte) (byte, byte) {
 	num1 := B >> 4
 	num2 := B << 4
 	num2 = num2 >> 4
 	return num1, num2
 }
 
+//CreateNibbleArray - Creates a byte array that stores merged nibbles that can be read by ReadNibbles()
 func CreateNibbleArray(length int) []byte {
 	Array := make([]byte, length)
 	return Array
 }
 
+//AppendUnmergedNibblestoArray - Append 2 unmerged nibbles into a byte array for space savings
+func AppendUnmergedNibblestoArray(Array []byte, N1 byte, N2 byte) []byte {
+	N1 = MergeNibbles(N1, N2)
+	Array = append(Array, N1)
+	return Array
+}
+
+//AppendMergedNibbletoNibbleArray - Append a merged nibble to nibble array
+func AppendMergedNibbletoNibbleArray(Array []byte, N1 byte) []byte {
+	Array = append(Array, N1)
+	return Array
+}
+
+//ReadNibble1 - Reads the first nibble in a merged nibble byte
 func ReadNibble1(B byte) byte {
 	B = B >> 4
+	if B > 15 {
+		B = 15
+	}
 	return B
 }
 
+//ReadNibble2 - Reads the second nibble in a merged nibble byte
 func ReadNibble2(B byte) byte {
 	B = B << 4
 	B = B >> 4
+	if B > 15 {
+		B = 15
+	}
 	return B
+}
+
+//MergeNibbles - Merges 2 non-merged nibbles togther into a byte
+func MergeNibbles(N1 byte, N2 byte) byte {
+	N1 = N1 << 4
+	N3 := N1 | N2
+	return N3
 }
